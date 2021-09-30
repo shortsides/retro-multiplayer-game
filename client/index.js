03eb90a8a2eb20911cbee111c8ca518f440848db
Duncan Milne <duncanadmilne@gmail.com>
2021-09-30 17:07:50 +1000

    separated client and server

--- client/index.js
+++ client/index.js
@@ -5,10 +5,10 @@ import SceneWorld from "./src/scenes/World.js";
 import MiniGameSnake from "./src/scenes/minigames/snake.js";
 
 // production server config
-//export const socket = io('https://agile-meadow-49870.herokuapp.com/');
+export const socket = io('https://agile-meadow-49870.herokuapp.com/');
 
 // local testing config
-export const socket = io('http://localhost:3000/');
+//export const socket = io('http://localhost:3000/');
 
 socket.on('loginSuccess', initGame);
 socket.on('nameTaken', handleNameTaken);
